344df309ff3ba011d4430e782e97e62d755cddb7
Edwin Rivera <edwin.rivera@outlook.com>
2017-02-18 15:46:54 -0500

    enabled skip buttons

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,8 +4,8 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 // build functions
 function togglePlay() {
@@ -21,9 +21,15 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
 // hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
